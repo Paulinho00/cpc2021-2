416c0f029df4e4606f30375043361459db846e48
Paweł Gryglewicz <259136@student.pwr.edu.pl>
2021-11-25 18:10:29 +0100

    Laboratorium 3: praca domowa: Dodanie funkcjonalności usuwania zadań i ich edytowania

---
+++ laboratorium 3/praca domowa/PawełGryglewiczLab3PracDom/PawełGryglewiczLab3PracDom/scripts/creatorEditScript.js
@@ -0,0 +1,124 @@
+﻿
+//Tablica przechowująca dane z pól tekstowych
+let inputs =[];
+
+//Funkcja startowa strony kreatora
+function Start() {
+    //Dodanie eventListenra do przycisku cofającego na główną stronę
+    document.getElementById("back-to-main-page").addEventListener("click", BackToMainPage);
+    //Sprawdzenie czy jest włączona opcja tworzenia
+    if (window.sessionStorage.getItem("editOrCreate") === "create") {
+        //Dodanie eventListnera do przycisku potwierdzeni
+        document.getElementById("confirm-values").addEventListener("click", CreateTask);
+    }
+
+    //Sprawdzenie czy jest włączona opcja edycji
+    if (window.sessionStorage.getItem("editOrCreate") === "edit") {
+
+        //Pobranie indeksu edytowanego zadania
+        let index = window.sessionStorage.getItem("chosenIndex")
+        //Pobranie danych edytowanego zadania
+        let storage = JSON.parse(window.sessionStorage.getItem(index));
+        //Wyświetlenie danych zadania w polach tekstowych
+        BaseValues(storage);
+        //Dodanie eventListnera do przycisku potwierdzenia
+        document.getElementById("confirm-values").addEventListener("click", function () { EditValues(index) });
+
+    }
+}
+
+//Funkcja tworząca nowe zadanie i zapisująca je w pamięci sesji
+function CreateTask() {
+
+    //Wywołanie metody zczytującej wartości w polach tekstowych
+    ReadValues();
+    
+    //Wyczyszczenie pól tesktowych
+    document.getElementById("name-input").value = " ";
+    document.getElementById("description-input").value = " ";
+    document.getElementById("date-input").value = " ";
+    document.getElementById("priority").value = " ";
+    document.getElementById("duration-input").value = " ";
+
+    //Zapisanie danych w sesji przeglądarki
+    window.sessionStorage.setItem(window.sessionStorage.getItem("counter") + 1, JSON.stringify(inputs));
+    //Zwiększenie licznika
+    window.sessionStorage.setItem("counter", window.sessionStorage.getItem("counter") + 1)
+    
+}
+//Funkcja edytująca zadanie o danym indeksie
+function EditValues(index) {
+    //Wywołanie metody zczytującej wartości w polach tekstowych
+    ReadValues();
+    //Zapisanie zaaktualizowanych danych w pamięci sesji
+    window.sessionStorage.setItem(index, JSON.stringify(inputs));
+    //Wywołanie metody otwierająca główną stronę
+    BackToMainPage();
+}
+
+
+//Powrót do głównej strony
+function BackToMainPage() {
+    window.open("../index.html", "_self");
+}
+
+//Licznik id wpisów do pamięci sesji
+function StartCounter() {
+    window.sessionStorage.setItem("counter", 0);
+}
+
+// Funkcja wyświetlająca aktualne dane wybranego zadania w polu tekstowym
+function BaseValues(storage) {
+    document.getElementById("name-input").value = storage[0];
+    document.getElementById("description-input").value = storage[1];
+    document.getElementById("date-input").value = storage[2];
+    document.getElementById("priority").value = storage[3];
+    document.getElementById("duration-input").value = storage[4];
+}
+
+//Funkcja odczytująca wartości w polach tekstowych
+function ReadValues() {
+    //Sprawdzenie czy pole jest puste
+    if (document.getElementById("name-input") === null || document.getElementById("name-input").value === "") {
+        alert("Wypełnij wszystkie pola!");
+        return;
+    }
+    //Odczyt nazwy zadania
+    inputs[0] = document.getElementById("name-input").value;
+
+    //Sprawdzenie czy pole jest puste
+    if (document.getElementById("description-input") === null || document.getElementById("description-input").value === "") {
+        alert("Wypełnij wszystkie pola!");
+        return;
+    }
+    //Odczyt opisu zadania
+    inputs[1] = document.getElementById("description-input").value;
+
+    //Sprawdzenie czy pole jest puste
+    if (document.getElementById("date-input") === null || document.getElementById("date-input").value === "") {
+        alert("Wypełnij wszystkie pola!");
+        return;
+    }
+    //Odczyt daty wykonania zadania
+    inputs[2] = document.getElementById("date-input").value;
+
+    
+    //Sprawdzenie czy pole jest puste
+    let select = document.getElementById("priority");
+    if (select.options[select.selectedIndex].value === "") {
+        alert("Wypełnij wszystkie pola!");
+        return;
+    }
+    //Odczyt priorytetu zadania
+    inputs[3] = document.getElementById("priority").value;
+
+    //Sprawdzenie czy pole jest puste
+    if (document.getElementById("duration-input") === null || document.getElementById("duration-input").value === "") {
+        alert("Wypełnij wszystkie pola!");
+        return;
+    }
+    //Odczyt czasu trwania zadania
+    inputs[4] = document.getElementById("duration-input").value;
+
+}
+
---
+++ laboratorium 3/praca domowa/PawełGryglewiczLab3PracDom/PawełGryglewiczLab3PracDom/scripts/mainPageScripts.js
@@ -0,0 +1,104 @@
+﻿
+
+//Funkcja przypisująca EventListenery do przycisków na głównej stronie
+function ButtonInit() {
+    document.getElementById("open-creator-page").addEventListener("click", OpenCreatorPage);
+    document.getElementById("open-edit-page").addEventListener("click", OpenEditPage);
+}
+
+//Funkcja otwierająca stronę 
+function OpenCreatorPage() {
+    //Otwarcie strony i ustawienie odpowiedniej opcji w pamięci sesji
+    window.sessionStorage.setItem("editOrCreate", "create")
+    window.open("./pages/new-task-creator-page.html", "_self");
+}
+
+// Funkcja usuwająca zadanie o podanym indeksie
+function DeleteTask(index) {
+    //Usunięcie elementu z pliku html
+    document.getElementById("task" + index).remove();
+    //Usunięcie danych o zadaniu z pamięci sesji
+    window.sessionStorage.removeItem(index);
+    //Zresetowanie searchboxa
+    ResetSelect();
+    //Ponowne załadowanie listy
+    ReloadTaskList();
+}
+
+// Funkcja zmieniają styl wyświetlania zadania
+function MarkAsDone(index) {
+    // Odczyt nagłówka z plik html
+    let li = document.getElementById("task" + index);
+
+    //Zmiana stylu
+    li.style.backgroundColor = "grey";
+    li.getElementsByTagName('span')[0].style.textDecoration = "line-through";
+
+    //Usunięcie przycisku 
+    document.getElementById("marks-as-done-task" + index).remove();
+}
+
+// Funkcja aktualizująca wyświetlane dane na głównej stronie
+function ReloadTaskList() {
+    //Sprawdzenie czy pamięc sesji nie jest pusta
+    if (window.sessionStorage.length !== 0) {
+        //Wywołanie pętli do odczytu całej pamięci sesji
+        for (let i = 1; i <= window.sessionStorage.getItem("counter"); i++) {
+            //Sprawdzenie czy dany wpis w pamięci nie jest nullem
+            if (window.sessionStorage.getItem(i) !== null) {
+                //Odczyt tablicy stringów
+                let storage = JSON.parse(window.sessionStorage.getItem(i));
+                //Dodanie wartości do datelist searchboxa
+                AddElementToSelect(storage);
+                //Stworzenie nowego elementu
+                let li = document.createElement("li");
+                //Ustawienie atrybutów nowego elementu
+                li.setAttribute("id", "task" + i);
+                li.setAttribute("class", "task");
+                //Dodanie odpowiednich nagłówków i wartości do nowego elementu
+                li.innerHTML += '<span class="task-text">' + storage[0] + '</span>' +
+                    '<button id="marks-as-done-task' + i + '" type="button" class="small-normal button">Wykonane</button>' +
+                    '<button id="delete-task' + i + '" type="button" class="delete button">Usuń</button>';
+
+                //Dodanie nowego elementu do pliku html
+                document.getElementById("tasks").appendChild(li);
+
+                //Dodanie actionListenerów do przycisków w nowym elemencie
+                document.getElementById("marks-as-done-task" + i).addEventListener("click", function () { MarkAsDone(i) });
+                document.getElementById("delete-task" + i).addEventListener("click", function () { DeleteTask(i) });
+            }
+        }
+    }
+}
+
+// Funkcja resetujaca searchboxa
+function ResetSelect() {
+    document.getElementById("searchbox").innerHTML = "";
+}
+// Funkcja dodająca zadanie do searchboxa
+function AddElementToSelect(inputs) {
+    //Utworzenie nowej opcji
+    let option = document.createElement("option");
+    //Ustawienie wartości nowej opcji
+    option.innerHTML += inputs[0];
+    //Dodanie opcji do datelist
+    document.getElementById("searchbox").appendChild(option);
+}
+
+// Funkcja otwierająca stronę edycji zadania
+function OpenEditPage() {
+    //Pobranie nazwy wybranego zadania
+    let taskName = document.getElementById("searchbox").value;
+    //Pętla sprawdzająca jaki indeks ma dane zadanie
+    for (let i = 0; i <= window.sessionStorage.getItem("counter"); i++) {
+        //Sprawdzenie czy nazwa zadani się zgadza i czy dany indeks nie jest nullem
+        if (window.sessionStorage.getItem(i) !== null && (JSON.parse(window.sessionStorage.getItem(i)))[0] === taskName) {
+            //Zapisanie indeksu wybranego zadania
+            window.sessionStorage.setItem("chosenIndex", i);
+        }
+    }
+    //Otwarcie strony i ustawienie odpowiedniej opcji w pamięci sesji
+    window.sessionStorage.setItem("editOrCreate", "edit")
+    window.open("./pages/new-task-creator-page.html", "_self");
+}
+
