a4dc20ff6f664d5be3330a2e43b68a094f895320
Paweł Gryglewicz <259136@student.pwr.edu.pl>
2021-12-01 17:56:01 +0100

    Laboratorium 4: praca domowa: Dodanie wyglądu strony logowania

---
+++ laboratorium 4/praca domowa/PawełGryglewiczLab4PracDom/PawełGryglewiczLab4PracDom/Models/UserData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PawełGryglewiczLab4PracDom.Models
+{
+    /// <summary>
+    /// Klasa przechowująca dane logowania
+    /// </summary>
+    public class UserData
+    {
+        //Adres email
+        public string Email { get; set; }
+        //Hasło
+        public string Password { get; set; }
+    }
+}
