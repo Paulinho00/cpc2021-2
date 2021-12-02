f391ba121b391a1cbaa3291839c0d8f5de24dc7c
Paweł Gryglewicz <259136@student.pwr.edu.pl>
2021-12-02 11:56:44 +0100

    Laboratorium 4: praca domowa: Dodanie klasy Transfer

---
+++ laboratorium 4/praca domowa/PawełGryglewiczLab4PracDom/PawełGryglewiczLab4PracDom/Models/Transfer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PawełGryglewiczLab4PracDom.Models
+{
+    /// <summary>
+    /// Klasa reprezentująca przelew
+    /// </summary>
+    public class Transfer
+    {
+        /// <summary>
+        /// Tytuł przelewu
+        /// </summary>
+        public string Title { get; set; }
+        /// <summary>
+        /// Kwota przelewu
+        /// </summary>
+        public float Sum { get; set; }
+        /// <summary>
+        /// Data przelewu
+        /// </summary>
+        public string Date { get; set; }
+
+        public Transfer(string title, float sum, string date)
+        {
+            Title = title;
+            Sum = sum;
+            Date = date;
+        }
+
+    }
+}
