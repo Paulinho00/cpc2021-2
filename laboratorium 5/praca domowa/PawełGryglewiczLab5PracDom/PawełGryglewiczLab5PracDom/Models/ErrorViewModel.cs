24082e121f34542d68883422b8b8f1250308aa0c
Paweł Gryglewicz <259136@student.pwr.edu.pl>
2021-12-08 11:29:50 +0100

    Laboratorium 5: praca domowa: inicjalizacja projektu

---
+++ laboratorium 5/praca domowa/PawełGryglewiczLab5PracDom/PawełGryglewiczLab5PracDom/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PawełGryglewiczLab5PracDom.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
