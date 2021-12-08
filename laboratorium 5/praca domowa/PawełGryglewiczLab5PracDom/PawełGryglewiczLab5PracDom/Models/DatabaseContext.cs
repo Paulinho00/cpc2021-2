bdc056a5060eb91590a00476b2495c146d67d93a
Paweł Gryglewicz <259136@student.pwr.edu.pl>
2021-12-08 19:31:47 +0100

    Laboratorium 5: praca domowa: Dodanie metod w klasie DatabaseContext
    Dodanie metod reprezentujących poszczególne tabele w bazie danych

--- laboratorium 5/praca domowa/PawełGryglewiczLab5PracDom/PawełGryglewiczLab5PracDom/Models/DatabaseContext.cs
+++ laboratorium 5/praca domowa/PawełGryglewiczLab5PracDom/PawełGryglewiczLab5PracDom/Models/DatabaseContext.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace PawełGryglewiczLab5PracDom.Models
@@ -11,10 +12,29 @@ namespace PawełGryglewiczLab5PracDom.Models
     /// </summary>
     public class DatabaseContext : DbContext
     {
-        public DatabaseContext(DbContextOptions<DatabaseContext> options): base(options)
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
         {
-
+  
         }
 
+        /// <summary>
+        /// Tabela ze stacjami
+        /// </summary>
+        public DbSet<Station> Stations { get; set; }
+
+        /// <summary>
+        /// Tabela z pociągami
+        /// </summary>
+        public DbSet<Train> Trains { get; set; }
+
+        /// <summary>
+        /// Tabela z połączeniami kolejowymi
+        /// </summary>
+        public DbSet<RailwayConnection> RailwayConnections { get; set; }
+
+        /// <summary>
+        /// Tabela z pasażerami
+        /// </summary>
+        public DbSet<Passenger> Passengers { get; set; }
     }
 }
