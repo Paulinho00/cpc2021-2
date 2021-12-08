08233693bc5ca809ce9456dff91faaa25b8cb971
Paweł Gryglewicz <259136@student.pwr.edu.pl>
2021-12-08 12:05:58 +0100

    Laboratorium 5: praca domowa: Dodanie modelu bazy danych i połączenia z bazą danych

---
+++ laboratorium 5/praca domowa/PawełGryglewiczLab5PracDom/PawełGryglewiczLab5PracDom/Models/DatabaseContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PawełGryglewiczLab5PracDom.Models
+{
+    /// <summary>
+    /// Klasa będąca modelem bazy danych
+    /// </summary>
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options): base(options)
+        {
+
+        }
+
+    }
+}
