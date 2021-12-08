f4654ee61426c72b04ad0bc35ffc63bfd4eb1d55
Paweł Gryglewicz <259136@student.pwr.edu.pl>
2021-12-08 17:16:26 +0100

    Laboratorium 5: praca domowa: dodanie modelu połączenia kolejowego

---
+++ laboratorium 5/praca domowa/PawełGryglewiczLab5PracDom/PawełGryglewiczLab5PracDom/Models/RailwayConnection.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PawełGryglewiczLab5PracDom.Models
+{
+    /// <summary>
+    /// Klasa reprezentująca połączenie kolejowe
+    /// </summary>
+    public class RailwayConnection
+    {
+        /// <summary>
+        /// Klucz główny
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Czas odjazdu
+        /// </summary>
+        [Required]
+        public DateTime TimeOfDeparture { get; set; }
+
+        /// <summary>
+        /// Id stacji początkowej
+        /// </summary>
+        [Required]
+        public int PlaceOfDepartureId { get; set; }
+
+        /// <summary>
+        /// Czas przyjazdu
+        /// </summary>
+        [Required]
+        public  DateTime TimeOfArrival { get; set; }
+
+        /// <summary>
+        /// Id stacji końcowej
+        /// </summary>
+        [Required]
+        public int DestinationId { get; set;}
+
+        /// <summary>
+        /// Id pociągu
+        /// </summary>
+        [Required]
+        public int TrainId { get; set; }
+
+        /// <summary>
+        /// Obiekt stacji początkowej
+        /// </summary>
+        [ForeignKey("PlaceOfDepartureId")]
+        public Station PlaceOfDeparture { get; set; }
+
+        /// <summary>
+        /// Obiekt stacji końcowej
+        /// </summary>
+        [ForeignKey("DestinationId")]
+        public Station Destination { get; set; }
+
+        /// <summary>
+        /// Obiekt pociągu
+        /// </summary>
+        [ForeignKey("TrainId")]
+        public Train Train { get; set; }
+
+    }
+}
