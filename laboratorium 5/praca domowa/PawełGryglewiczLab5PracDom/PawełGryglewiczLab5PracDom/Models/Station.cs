0a2d17486702f606a5e3bac3754840bb325dfcb9
Paweł Gryglewicz <259136@student.pwr.edu.pl>
2021-12-08 12:22:37 +0100

    Laboratorium 5: praca domowa: dodanie modelu stacji kolejowej

---
+++ laboratorium 5/praca domowa/PawełGryglewiczLab5PracDom/PawełGryglewiczLab5PracDom/Models/Station.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PawełGryglewiczLab5PracDom.Models
+{
+    /// <summary>
+    /// Klasa reprezentująca stację kolejową
+    /// </summary>
+    public class Station
+    {
+        /// <summary>
+        /// Id stacji
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Miejscowość stacji
+        /// </summary>
+        [MaxLength(25)][Required]
+        public string Town { get; set; }
+
+        /// <summary>
+        /// Liczba peronów na stacji
+        /// </summary>
+        [Required]
+        public int PlatformsNumber { get; set; }
+
+        /// <summary>
+        /// Rok otwarcia stacji
+        /// </summary>
+        [Required]
+        public int YearOfFound { get; set; }
+
+        /// <summary>
+        /// Zmienna przechowująca informację czy stacja ma kasy biletowe
+        /// </summary>
+        [Required]
+        public bool HasTicketOffice { get; set; }
+    }
+}
