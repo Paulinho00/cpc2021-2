449a7da876aa6d3561fbd4253584503c6cc17e00
Paweł Gryglewicz <259136@student.pwr.edu.pl>
2021-12-22 09:36:28 +0100

    Lab6: Dodanie pracy z zajęć

---
+++ laboratorium 6/praca z zajęć/PawełGryglewiczLab6/PawełGryglewiczLab6/Controllers/PizzaController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PawełGryglewiczLab6.Models;
+using PawełGryglewiczLab6.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PawełGryglewiczLab6.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PizzaController: ControllerBase
+    {
+        private IPizzaService _pizzaService;
+
+        public PizzaController(IPizzaService pizzaService)
+        {
+            _pizzaService = pizzaService;
+        }
+
+        [HttpGet]
+        [Produces(typeof(List<Pizza>))]
+        public IActionResult GetAllPizza() 
+        {
+            var pizzas = _pizzaService.Get();
+            return Ok(pizzas);
+        }
+
+        [HttpPost]
+        [Produces(typeof(int))]
+        public IActionResult Post([FromBody] Pizza pizza)
+        {
+            int id = _pizzaService.Post(pizza);
+            return Ok(id);
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+
+        public IActionResult Put([FromRoute] int id, [FromBody] Pizza pizza)
+        {
+            if(id != pizza.Id)
+            {
+                return Conflict("Podane id są różne");
+            }
+
+            var isUpdateSuccesfull = _pizzaService.Put(id, pizza);
+
+            if (isUpdateSuccesfull) 
+            {
+                return NoContent();
+            }
+            return NotFound();
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            var result = _pizzaService.Delete(id);
+            if (result)
+                return NoContent();
+
+            return NotFound();
+        }
+    }
+            
+}
---
+++ laboratorium 6/praca z zajęć/PawełGryglewiczLab6/PawełGryglewiczLab6/Services/IPizzaService.cs
@@ -0,0 +1,39 @@
+﻿using PawełGryglewiczLab6.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PawełGryglewiczLab6.Services
+{
+    public interface IPizzaService
+    {
+        /// <summary>
+        /// Metoda zwraca wszytkie pizze
+        /// </summary>
+        /// <returns></returns>
+        List<Pizza> Get();
+
+        /// <summary>
+        /// Metoda dodaje nową pizzę i zwraca jej id
+        /// </summary>
+        /// <param name="pizza"></param>
+        /// <returns></returns>
+        int Post(Pizza pizza);
+
+        /// <summary>
+        /// Metoda edytuje wskazaną pizze 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="pizza"></param>
+        /// <returns></returns>
+        bool Put(int id, Pizza pizza);
+
+        /// <summary>
+        /// Metoda usuwa wskazaną pizze
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        bool Delete(int id);
+    }
+}
