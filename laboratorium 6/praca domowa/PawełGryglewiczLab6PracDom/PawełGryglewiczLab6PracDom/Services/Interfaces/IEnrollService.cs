52cbfb0b9a4c4e6040350bf673e222d926d79ebc
Paweł Gryglewicz <259136@student.pwr.edu.pl>
2022-01-06 11:55:32 +0100

    Lab6: pracdom: Dodanie interfejsu serwisu obsługującego zapisy studenta na zajęcia

---
+++ laboratorium 6/praca domowa/PawełGryglewiczLab6PracDom/PawełGryglewiczLab6PracDom/Services/Interfaces/IEnrollService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PawełGryglewiczLab6PracDom.Services.Interfaces
+{
+    public interface IEnrollService
+    {
+
+        /// <summary>
+        /// Metoda usuwająca zapis studenta na dane zajęcia
+        /// </summary>
+        /// <param name="studentId">Id studenta</param>
+        /// <param name="lessonId">Id zajęć</param>
+        /// <returns></returns>
+        bool Delete(int studentId, int lessonId);
+
+        /// <summary>
+        /// Metoda zapisująca studenta na dane zajęcia
+        /// </summary>
+        /// <param name="studentId">Id studenta</param>
+        /// <param name="lessonId">Id zajęć</param>
+        /// <returns></returns>
+        int Post(int studentId, int lessonId);
+
+        /// <summary>
+        /// Metoda edytujaca zapis danego studenta na zajęcia
+        /// </summary>
+        /// <param name="studentId">Id studenta</param>
+        /// <param name="lessonId">Id zajęć</param>
+        /// <returns></returns>
+        int Put(int studentId, int lessonId);
+    }
+}
