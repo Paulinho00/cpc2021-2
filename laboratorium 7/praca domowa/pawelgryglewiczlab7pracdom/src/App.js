c0d300dda92ed067ea9a3abdea04a8f1086711d0
Paweł Gryglewicz <259136@student.pwr.edu.pl>
2022-01-13 10:31:58 +0100

    Lab7: pracdom: dodanie paska nawigacyjnego

--- laboratorium 7/praca domowa/pawelgryglewiczlab7pracdom/src/App.js
+++ laboratorium 7/praca domowa/pawelgryglewiczlab7pracdom/src/App.js
@@ -1,11 +1,36 @@
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Nav, NavLink, NavItem } from 'reactstrap';
+import { useState } from 'react';
+
+
 
 function App() {
+
+  //Stan przechowujący informację którą stronę wygenerować
+  const [page, setPage] = useState(0);
+
+
+  //Funkcja zmieniają wyświetlanie zakładki
+  const changePage = function(page){
+    setPage(page);
+  }
+
   return (
-    <div className="App">
-      </div>
+    <div className='App'>
+      <Nav style={{backgroundColor: '#494949'}} tabs>
+        <NavItem>
+          <NavLink style={{color: 'white'}} onClick={() =>changePage(0)}>Strona główna</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink style={{color: 'white'}} onClick={() =>changePage(1)}>Dodaj osobę</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink style={{color: 'white'}} onClick={() =>changePage(2)}>Lista osób</NavLink>
+        </NavItem>
+      </Nav>
+    </div>
   );
 }
 
