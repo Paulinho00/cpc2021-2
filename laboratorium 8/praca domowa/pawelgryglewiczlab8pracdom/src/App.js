6f148c4e2d15223867fd3beb7946267fc5e7cd7e
Paweł Gryglewicz <259136@student.pwr.edu.pl>
2022-01-20 10:19:01 +0100

    Lab8: pracdom: dodanie paska nawigacyjnego w głównym widoku

--- laboratorium 8/praca domowa/pawelgryglewiczlab8pracdom/src/App.js
+++ laboratorium 8/praca domowa/pawelgryglewiczlab8pracdom/src/App.js
@@ -1,11 +1,25 @@
 
 import './App.css';
+import React from 'react';
+import {Menu} from 'antd';
+import {CaretDownOutlined} from "@ant-design/icons"
+import SubMenu from 'antd/lib/menu/SubMenu';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+        <Menu  mode="horizontal" theme="dark">
+          <Menu.Item key="main page">
+            Strona główna
+          </Menu.Item>
+          <Menu.Item key="SubMenu-Item">
+            <SubMenu key="SubMenu" icon={<CaretDownOutlined />} title="Opcje">
+              <Menu.Item key="addPost">Dodaj post</Menu.Item>
+              <Menu.Item key="findPostById">Szukaj</Menu.Item>
+            </SubMenu>
+          </Menu.Item>
+        </Menu>
     </div>
   );
 }
