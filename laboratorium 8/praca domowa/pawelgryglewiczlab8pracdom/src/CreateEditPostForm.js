1266d8892add639cb5ac530c5b5d35f19930ddee
Paweł Gryglewicz <259136@student.pwr.edu.pl>
2022-01-22 10:50:53 +0100

    Lab8: pracdom: dodanie zabezpieczeń przed wprowadzeniem pustych pól w formularzu

--- laboratorium 8/praca domowa/pawelgryglewiczlab8pracdom/src/CreateEditPostForm.js
+++ laboratorium 8/praca domowa/pawelgryglewiczlab8pracdom/src/CreateEditPostForm.js
@@ -1,22 +1,43 @@
 import React from 'react';
-import {Form, InputNumber, Input} from 'antd';
+import { Form, InputNumber, Input } from 'antd';
 
 //Formularz edycji/tworzenia postu
-function CreateEditPostForm({initialValues, form}){
+function CreateEditPostForm({ initialValues, form }) {
 
-    return(
+    return (
         <Form form={form} layout='horizontal' initialValues={initialValues}>
             <Form.Item name='id' hidden >
-                <Input/>
+                <Input />
             </Form.Item>
-            <Form.Item label="Id autora postu: " name="userId">
-                <InputNumber size="300" min={1} max={10} defaultValue={1}/>
+            <Form.Item label="Id autora postu: " name="userId"
+                rules={[
+                    {
+                        required: true,
+                        message: "Wprowadź id autora postu"
+                    }
+                ]}
+            >
+                <InputNumber size="300" min={1} max={10} />
             </Form.Item>
-            <Form.Item label="Tytuł: " name="title">
-                <Input/>
+            <Form.Item label="Tytuł: " name="title"
+                rules={[
+                    {
+                        required: true,
+                        message: "Wprowadź tytuł postu"
+                    }
+                ]}
+            >
+                <Input />
             </Form.Item>
-            <Form.Item label="Treść: " name="body">
-                <Input/>
+            <Form.Item label="Treść: " name="body"
+                rules={[
+                    {
+                        required: true,
+                        message: "Wprowadź treść postu"
+                    }
+                ]}
+            >
+                <Input />
             </Form.Item>
         </Form>
     );
